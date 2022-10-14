04a4c1ba24b59c10e01e4343baa2e9b87bc5b2e4
Janreign <janreign.tibs@gmail.com>
2022-10-14 16:31:07 +0800

    Initial Commit

---
+++ src/main/kotlin/Activity1A/Activity _1A_ A.kt
@@ -0,0 +1,37 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.Date
+
+fun main () {
+
+         //Student Information
+
+    var studentName: String? = "Janreign Sample."
+    var batch: String? = "MD2P"
+    val cellPhoneNumber: Long? = null
+    var studentEmail: String? = null
+
+     // Activities and Assignments
+
+    var assignmentName: String = "Sample Assignment Name"
+    var assignmentStatus: String? = null
+    var assignmentMissed: String? = null
+    var assignmentGrades: Int? = null
+
+    var activityName: String = "Sample Activity Name"
+    var activityStatus: String? = null
+    var activtyMissed: String? = null
+    var activityGrades: Int? = null
+
+
+    // Student Status
+
+    var dateEnrolled: String? = null
+    var absences: Int? = null
+    var numberOfLates: Int? = null
+    var isDropped: Boolean? = null
+    var lastDayOfClass: String? = null
+    var hadReadAnnouncements: Boolean? = null
+
+
+}
