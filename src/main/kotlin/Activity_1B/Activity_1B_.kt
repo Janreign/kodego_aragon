2f96fdad405b288f2ee2c1ccdcb5e04434b94e5a
Janreign <janreign.tibs@gmail.com>
2022-10-14 16:32:45 +0800

    Initial Commit

---
+++ src/main/kotlin/Activity_1B/Activity_1B_.kt
@@ -0,0 +1,36 @@
+fun main () {
+
+
+    //You are tasked to automate a school’s library//
+
+    //List of Five Items you can borrow in a Library//
+
+   var item1Book: String = "Book"
+   var bookTitle: String = "Sample Title: The Alchemist"
+   var authorName: String= "Sample auAuthor: Paolo Coelho"
+   var bookMaterialType: String = "Sample Material: Paperback"
+   val bookSize: String = "Sample Size: 4.57 x 0.91 x 6.18 inches"
+   var ISBN: Long? = null
+
+   var item2WorldMap: String = "World Map from USA"
+   var Material: String = "Sample: Plastic"
+   var from: String = "Sample: USA"
+   val Size = "Sample:166 x 77"
+
+   var item3MusicalInstrument: String? = null
+   var instrumentType = "Sample: Violin"
+   var material:String = "Sample: Maple Wood"
+   val size:String = "Sample: 36 cm"
+
+   var item4Artwork:String = "Artwork"
+   var artWorkName: String? = "Sample Artwork Name"
+   var artist: String? = null
+   var typeOfArt: String? = null
+   var canvasSize: String? = null
+
+   var item5VCDDVD:String = "VCD/DVD"
+   var CdDvdTopic: String? = null
+   var categoryOrGenre: String? = null
+   var Director: String? = null
+
+}
