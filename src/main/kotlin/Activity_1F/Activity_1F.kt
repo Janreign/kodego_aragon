9eb137a40e15d23accedd8db4c81630a016822b4
Janreign <janreign.tibs@gmail.com>
2022-11-13 16:56:16 +0800

    Activity_1E_3 uses of Array, ArrayList, Set, Map.

---
+++ src/main/kotlin/Activity_1F/Activity_1F.kt
@@ -0,0 +1,28 @@
+//Create an application that will accept 2 string inputs.
+//Your application will print all unique characters in both Strings.
+//
+//
+//Scope :
+//String
+//Loops
+
+
+
+
+//import mu.KotlinLogging
+//
+//private val logger = KotlinLogging.logger {}
+//
+//fun main() {
+//
+//    var string1: String = readln()
+//    var string2: String = readln()
+//
+//}
+
+
+
+
+
+
+
