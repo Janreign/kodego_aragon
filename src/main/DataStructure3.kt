10c5f3c28706b72d66bd3eb5408a2598cbb80e78
Janreign <janreign.tibs@gmail.com>
2022-12-09 00:25:35 +0800

    Activity_1H

---
+++ src/main/DataStructure3.kt
@@ -0,0 +1,79 @@
+//import java.util.ArrayList
+//import java.util.LinkedHashSet
+//import java.util.LinkedList
+//
+//fun main () {
+//
+//    var set: Set<String> = HashSet<String>()
+//    var map: Map<String, String> = HashMap<String, String>
+//    var list: List<String> = ArrayList<String>()
+//
+//    var listNames = ArrayListP<String>()
+//    var listLocation = LinkedList<String>()
+//
+//
+//    var mapName = HashMap<String, String>
+//    var mapLocation = LinkHashmap<String, String>()
+//
+//    var setName: = HashSet<String>()
+//    var setLocation = LinkedHashSet<String>()
+//
+//    var set: Set<String> = HashSet<String>()
+//    var map: Map<String, String> = HashMap<String, String>
+//    var list: List<String> = ArrayList<String>()
+//
+//
+//    var Names: List<String> mutableListOf<String> ("Jason", "Kurt", "Logan", "Monique")
+//    var numbers: Set<Char> = mutableListOf('1', '2', '3', '4', '5')
+//    var characters: Map<String, Int> =
+//        mutableMapOf(("One" to 1), ("Two" to 2), ("Three" to 3)) // "to" should be color yellow, check the error
+//
+//    var Locations: List<String> = listOf("Cubao", "Santolan", "Shaw", "Boni", "Buendia")
+//    var setPoints: Set<Int> = setOf(1, 2, 3, 4, 5)
+//    var mapPoints: Map<String>, Int> = mapOf(("One" to 1), ("Two"to 2), ("Three" to 3))
+//
+//    var arrayList: Arraylist<Strings> = names as ArrayList<String>
+//    var linkedList: LinkedList<Strings> = names as LinkedList<String> (String)
+//
+//
+//
+//
+//    for (name in names) {
+//
+//        logger.info { "Name: $name" } // need pa idownload ung logger
+//    }
+//
+//    for (index in names.indices) {
+//        logger.info { "Name [$index]: ${names[index]}" }
+//    }
+//    var iterator = name.iterator()
+//    while (iterator.hasNext())
+//        logger.info { "Name: ${iterator.next()}" }
+//
+//}
+//
+//names.forEach{it:String
+//
+//logger.info{"Name: $it"}
+//
+//nmaes.forEachIndexed { "$[index]: , $name" }
+//
+//
+//        }
+//
+//    }
+//
+//}
+//
+//-----------------------------------
+//
+//var studentnames = name as Arraylist
+//
+//studentnames.add("Noah")
+//studentnames.add(index: 0,element: "Isaiah")
+//studentnames.add(Oakley)
+//studentnames.addAll()
+//
+//
+//
+//
